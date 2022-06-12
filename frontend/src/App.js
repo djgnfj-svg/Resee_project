13d09b3500cea2d3daf6f70d8d05f8be37f3ddd4
djgnfj-svg <djgnfj8923@naver.com>
2022-06-12 13:30:18 +0900

    add login_temp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
-import MyNavbar from './Nav';
+import MyNavbar from './components/Nav';
 
 function App() {
 	return (
 		<div className="App">
 			<MyNavbar />
-			<div class="hero-image">
-				<div class="hero-text">
-					<h1>I am John Doe</h1>
-					<p>And I'm a Photographer</p>
-					<button class='hero-button'>Hire me</button>
-				</div>
-			</div>
 		</div>
 	);
 }
---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,45 @@
+import {Navbar, Container, Nav,} from 'react-bootstrap';
+import { Routes, Route, Link, useNavigate,BrowserRouter } from 'react-router-dom';
+import App from '../App';
+import Login from '../router/Login';
+
+function MyNavbar() {
+  let navigate = useNavigate();
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand onClick={()=>navigate('/')}>ReSee</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+          <Nav.Link onClick={()=>navigate('/')}>템플릿</Nav.Link>
+          <Nav.Link onClick={()=>navigate('/sharing')}>공유</Nav.Link>
+          <Nav.Link onClick={()=>navigate('/board')}>계시판</Nav.Link>
+          </Nav>
+          <Nav>
+          <Nav.Link onClick={()=>navigate('/login')}>로그인</Nav.Link>
+          <Nav.Link onClick={()=>navigate('/register')}>회원가입</Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Routes>
+          <Route path="/" element={
+              <div class="hero-image">
+              <div class="hero-text">
+                <h1>I am John Doe</h1>
+                <p>And I'm a Photographer</p>
+                <button class='hero-button'>Hire me</button>
+              </div>
+            </div>
+            }/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="*" element={<div>test</div>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default MyNavbar;
