969a8cafe1838e218894895b926bb0035f812f55
djgnfj-svg <djgnfj8923@naver.com>
2022-06-22 04:45:28 +0900

    add ReviewViewSet

--- backend/api/ViewSets/PostViewSet.py
+++ backend/api/ViewSets/PostViewSet.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status, exceptions
 from rest_framework.response import Response
 
@@ -34,7 +35,7 @@ class PostViewSet(viewsets.ModelViewSet):
 			userid = int(request.session.get("user"))
 		else:
 			userid = getUserId(request.user)
-		temp = ReviewPost.objects.get(id=pk).user_id
+		temp = get_object_or_404(ReviewPost, id=pk).user_id
 		if temp != userid:
 			raise exceptions.PermissionDenied({"msg" : "권한없음"}, code=status.HTTP_403_FORBIDDEN)
 		return super().retrieve(request, *args, **kwargs)
@@ -44,7 +45,7 @@ class PostViewSet(viewsets.ModelViewSet):
 			userid = int(request.session.get("user"))
 		else:
 			userid = getUserId(request.user)
-		temp = ReviewPost.objects.get(id=pk).user_id
+		temp = get_object_or_404(ReviewPost, id=pk).user_id
 		if temp != userid:
 			raise exceptions.PermissionDenied({"msg" : "권한없음"}, code=status.HTTP_403_FORBIDDEN)
 		return super().destroy(request, *args, **kwargs)
--- backend/api/ViewSets/ReviewViewSet.py
+++ backend/api/ViewSets/ReviewViewSet.py
@@ -1,4 +1,42 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, exceptions
+from rest_framework.response import Response
+
+from api.Utils.getUser import getUserId
+from api.Seriailzers.PostSerializer import PostsSerializer
+
+from books.models import ReviewPost
+
 class ReviewViewSet(viewsets.ViewSet):
-	def list(self, request):
-		return None
+	def list(self, request, book_id):
+		if request.user.is_anonymous:
+			userid = int(request.session.get("user"))
+		else:
+			userid = getUserId(request.user)
+		review_list = ReviewPost.objects.filter(Book_id=book_id)
+		return_data = []
+		first_data = ReviewPost.objects.filter(Book_id = book_id, user_id = userid, review_count__lte=8).first()
+		base_time = first_data.created_at
+		for review in review_list[2:]:
+			past_days = (review.created_at - base_time).days
+			if past_days == 0 and review.review_count < 1:
+				return_data.append(review)
+			elif past_days > 1 and past_days < 3 and review.review_count < 2:
+				return_data.append(review)
+			elif past_days > 3 and past_days < 7 and review.review_count < 3:
+				return_data.append(review)
+			elif past_days > 7 and past_days < 14 and review.review_count < 4:
+				return_data.append(review)
+			elif past_days > 14 and past_days < 31 and review.review_count < 5:
+				return_data.append(review)
+			elif past_days > 31 and past_days < 93 and review.review_count < 6:
+				return_data.append(review)
+			elif past_days > 93 and past_days < 182 and review.review_count < 7:
+				return_data.append(review)
+			elif past_days > 182 and review.review_count < 8:
+				return_data.append(review)
+
+		review_data = {}
+		for i, data in enumerate(return_data):
+			review_data[i] = PostsSerializer(data).data
+
+		return Response(review_data, status=status.HTTP_200_OK)
