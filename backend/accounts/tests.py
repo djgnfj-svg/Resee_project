4dbee50ac75f850b3d79b351a5158924b3629124
djgnfj-svg <djgnfj8923@naver.com>
2022-09-20 19:36:19 +0900

    add test books

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
+from accounts.models import User
+
 # Create your tests here.
+class testUserModel(TestCase):
+    def setUp(self):
+        self.username = "testcate1"
+        self.email = "test@test.com"
+        self.password1 = "test@0830"
+        self.password2 = "test@0830"
+        self.signup_url = "/api/accounts/"
+        User.objects.create(username=self.username, email = self.email,
+        password=self.password1)
+
+    def test_label(self):
+        user = User.objects.get(id=1)
+        field_label = user._meta.get_field('username').verbose_name
+        self.assertEquals(field_label, "username")
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,10 @@
 from rest_framework import routers
 
-from api.ViewSets.Verification import VerificationView, EmailcheckView
-from api.ViewSets.BookViewset import BooksViewSet
-from api.ViewSets.PostImgViewset import PostImgViewSet
-from api.ViewSets.PostViewset import PostViewSet
-from api.ViewSets.ReviewViewset import ReviewViewSet
+from api.Views.Verification import VerificationView, EmailcheckView
+from api.Views.BookViewset import BooksViewSet
+from api.Views.PostImgViewset import PostImgViewSet
+from api.Views.PostViewset import PostViewSet
+from api.Views.ReviewViewset import ReviewViewSet
 
 
 router = routers.DefaultRouter()
@@ -12,6 +12,6 @@ router = routers.DefaultRouter()
 router.register(r'books', BooksViewSet, basename="books")
 router.register(r'books/(?P<book_id>\d+)/post', PostViewSet, basename="post")
 router.register(r'books/(?P<book_id>\d+)/review', ReviewViewSet, basename="review")
-router.register(r'verification', VerificationView, basename="verification",)
+# router.register(r'verification', VerificationView, basename="verification",)
 router.register(r'books/post/(?P<post_id>\d+)/imgs', PostImgViewSet, basename="postImg",)
 router.register(r'emailcheck', EmailcheckView, basename="emailcheck",)
