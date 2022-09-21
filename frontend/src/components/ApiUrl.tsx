930a1ed695022352bde66ad3bd42927a83757a9c
b-hyoung <youqlrqod@gmail.com>
2022-09-21 15:33:22 +0900

    fix  BookList ApiUrl

--- frontend/src/components/ApiUrl.tsx
+++ frontend/src/components/ApiUrl.tsx
@@ -14,7 +14,7 @@ export const CategoryDelete = (id: number) => `http://127.0.0.1:8000/api/books/$
 /* Books & Review */
 export const ReviewBooks = (id:number) => `http://127.0.0.1:8000/api/books/${id}/review/` // Books 리뷰(Get) , 리뷰 복습완료(Post)
 
-export const BooksListUrl = (id:number) => `http://127.0.0.1:8000/api/books/${id}/post/` // 책들 리스트
+export const BooksListUrl = (id:number) => `http://127.0.0.1:8000/api/books/${id}/posts/` // 책들 리스트
 export const BooksImageUpload = (id:number) => `http://127.0.0.1:8000/api/books/post/${id}/imgs/` //Books 이미지 업로드 (Post)
 export const BooksPostUrl = (id:number) => `http://127.0.0.1:8000/api/books/${id}/post/` // Books 포스트 생성(Post)
 export const BooksPostData = (id:number , postId:number) => `http://127.0.0.1:8000/api/books/${id}/post/${postId}/` // 책하나의 값(get) , 수정(put)
