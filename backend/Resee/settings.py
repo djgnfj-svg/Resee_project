7992664fada176f36ead2797990a30e5914cb922
django-svg <djgnfj8923@naver.com>
2022-10-26 17:06:25 +0900

    add docker build

--- backend/Resee/settings.py
+++ backend/Resee/settings.py
@@ -150,8 +150,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '..', 'frontend', 'build', 'static'),
 ]
 
-STATIC_URL = 'django_static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'django_static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
